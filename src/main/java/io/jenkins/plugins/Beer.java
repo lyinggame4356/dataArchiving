f740acbf19b34e1a9a1916f3df982be850dcf6ff
lyinggame4356 <lying.game4356@tutanota.com>
2024-03-08 14:09:38 +0200

    add new features

---
+++ src/main/java/io/jenkins/plugins/Beer.java
@@ -0,0 +1,54 @@
+package io.jenkins.plugins;
+
+import java.util.Random;
+
+import hudson.Extension;
+import hudson.model.UnprotectedRootAction;
+
+@Extension
+public class Beer implements UnprotectedRootAction {
+    private final Random random = new Random();
+
+    public String getQuote() {
+        return MESSAGES[random.nextInt(MESSAGES.length)];
+    }
+    
+    public String getImage() {
+        return IMAGES[random.nextInt(IMAGES.length)];
+    }
+    
+    public String getIconFileName() {
+        return "/plugin/beer/icon.png";
+    }
+
+    public String getDisplayName() {
+        return "We Need Beer";
+    }
+
+    public String getUrlName() {
+        return "beer";
+    }
+
+    private final String[] MESSAGES = {
+            "Having RVM problem",
+            "Maven is downloading the internet",
+            "Bundler is fetching the RubyGems index",
+            "Jenkins is restarting",
+            "Code is compiling",
+            "It's 5 o'clock somewhere",
+            "Launching Eclipse",
+            "Firing up Emacs",
+            "Compiling OpenOffice from source",
+            "Running Selenium tests",
+            "Starting the Android emulator"
+    };
+
+    private final String[] IMAGES = {
+            "beer.gif",
+            "bigbeer.jpg",
+            "man.jpg",
+            "starwars-beer.gif",
+            "drinking-beer.gif",
+            "pouring-beer.gif"
+    };
+}
